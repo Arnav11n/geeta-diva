1eb3e217fc75299a51b1906cee0cc90d472d662a
Arnav11n <91536262+Arnav11n@users.noreply.github.com>
2026-08-07 02:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,5 +351,6 @@ def favicon():
 #    app.run(debug=True, port=5000)
 
 if __name__ == '__main__':
-    # Host 0.0.0.0 is MANDATORY for Replit to expose the website!
-    app.run(host='0.0.0.0', port=5000)
+    # Fix: Dynamically grab the port Render assigns, or default to 5000 locally!
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
